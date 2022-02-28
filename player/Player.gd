8a9146e5121123f975f8c5cf0978210d578185da
RJPrinceNZ <rjprincenz@gmail.com>
2022-02-28 13:25:23 +1300

    Animation_near_completion
    Hitbox_problems

--- player/Player.gd
+++ player/Player.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 var animation_timer_fin = true
 export (int) var speed = 120
-export (int) var jump_speed = 180
+export (int) var jump_speed = -180
 export (int) var gravity = 400
+export (float) var friction = 10
+export (float) var acceration = 25
+
 
 var velocity = Vector2.ZERO
 
-enum state {IDLE, WALKING, JUMP}
+enum state {IDLE, WALKING, STARTJUMP, JUMP_MID, FALL, JUMP, JUMPFINISH}
 
 onready var player_state = state.IDLE
 
@@ -17,19 +20,51 @@ func _on_Animation_timer_timeout():
 	animation_timer_fin = true
 
 func update_animation(anim):
-	$AnimationPlayer.play(anim)
+	if velocity.x > 0:
+		$Sprite.flip_h = false
+	if velocity.x < 0:
+		$Sprite.flip_h = true
+	match(anim):
+		state.FALL:
+			$AnimationPlayer.play("FALL")
+		state.IDLE:
+			$AnimationPlayer.play("IDLE")
+		state.WALKING:
+			$AnimationPlayer.play("WALK")
+		state.JUMP:
+			$AnimationPlayer.play("JUMP")
 	
-func handle_state(state):
-	pass
+func handle_state(player_state):
+	match(player_state):
+		state.STARTJUMP:
+			velocity.y = jump_speed
 
 func get_input():
 	var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
 	if dir != 0:
-		velocity.x = move_toward(velocity.x, dir/speed, speed)
+		velocity.x = move_toward(velocity.x, dir*speed, acceration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, 0)
+		velocity.x = move_toward(velocity.x, 0, friction)
 func _physics_process(delta):
 	get_input()
-	if velocity.x == 0:
+	if velocity == Vector2.ZERO:
 		player_state = state.IDLE
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		player_state = state.STARTJUMP
+	elif velocity.x != 0:
+		player_state = state.WALKING
+	
+	if not is_on_floor():
+		if velocity.y < 0:
+			player_state = state.JUMP
+		if velocity.y == 0:
+			player_state = state.JUMP_MID
+		if velocity.y > 0:
+			player_state = state.FALL
+	
+	handle_state(player_state)
+	update_animation(player_state)
+	
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
 
