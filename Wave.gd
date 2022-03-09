b2c717f6a86548b0d8a9e2617fa32bb55487c101
RJPrinceNZ <rjprincenz@gmail.com>
2022-03-09 14:58:07 +1300

    updates

--- Wave.gd
+++ Wave.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Node2D
 
 onready var root_node = get_tree().root.get_child(0)
 onready var explosion = preload("res://player/Explosion.tscn")
@@ -16,11 +16,6 @@ func _ready():
 	
 func _physics_process(delta):
 	position += transform.x * v * delta
-	for index in get_slide_count():
-		var collision = get_slide_collision(index)
-		if collision.collider.is_in_group("Box"):
-			collision.collider.apply_central_impluse(-collision.normal)
-		
 
 func _on_Life_time_timeout():
 	var disappearing = true
@@ -34,7 +29,7 @@ func _on_Area2D_body_entered(body):
 	if not body.is_in_group("Player") and not body.is_in_group("Wave"):
 		var new_explosion = explosion.instance()
 		new_explosion.global_transform = $Position2D.global_transform
-		root_node.add_child(explosion)
+		root_node.add_child(new_explosion)
 		queue_free()
 
 
