7e6caf635a224620c068c8d1ff38f2c275df54a9
RJPrinceNZ <rjprincenz@gmail.com>
2022-03-15 14:40:11 +1300

    Door_update

--- Objects/Button.gd
+++ Objects/Button.gd
@@ -5,6 +5,7 @@ onready var door = get_parent().get_node("Door")
 #onready var animation_player = get_parent().get_node("AnimationPlayer")
 onready var state_machine = get_parent().get_node("AnimationTree").get("parameters/playback")
 onready var btn_animation_player = get_parent().get_node("ButtonAnimationPlayer")
+onready var lgt_animation_player = get_parent().get_node("LightAnimationPlayer")
 
 func _ready():
 	
@@ -15,7 +16,7 @@ func _on_Area2D_body_entered(body):
 		num_object += 1
 		if num_object >= 1:
 			btn_animation_player.play("Button_Pushed")
-			
+			lgt_animation_player.play("On")
 			state_machine.travel("Open")
 	
 
@@ -26,5 +27,6 @@ func _on_Area2D_body_exited(body):
 		
 		if num_object <= 0:
 			btn_animation_player.play("Button_Off")
+			lgt_animation_player.play("Off")
 			state_machine.travel("Closed")
 	
