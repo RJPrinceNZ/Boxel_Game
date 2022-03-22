8b289194edc6662ae020635351d107e9965e0e35
RJPrinceNZ <rjprincenz@gmail.com>
2022-03-23 10:59:33 +1300

    Window_resolution_and_menu_stuff

--- Menus/Title.gd
+++ Menus/Title.gd
@@ -9,7 +9,7 @@ func _on_Settings_pressed():
 
 func _on_Play_pressed():
 	print("Play")
-
+	get_tree().change_scene("res://levels/test_world_2.tscn")
 
 func _on_Credits_pressed():
 	print("Credits")
@@ -17,4 +17,5 @@ func _on_Credits_pressed():
 
 func _on_Quit_pressed():
 	print("Quit")
+	get_tree().quit()
 
