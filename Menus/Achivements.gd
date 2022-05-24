c1090e524323f2ef0f8ad9bc6cfbc9a0f76de635
RJPrinceNZ <rjprincenz@gmail.com>
2022-05-24 17:10:36 +1200

    Achievement_functions

--- Menus/Achivements.gd
+++ Menus/Achivements.gd
@@ -1,11 +1,20 @@
 extends Node
 
 var changing = false
+var Escaped = preload("res://sprites/Icon_Achivement_Escape.png")
+var Potato = preload("res://sprites/Icon_Achivement_Potato.png")
+var Missed = preload("res://sprites/Icon_Achivement_You_Missed.png")
 
 func _process(delta):
 	if changing == true:
 		if ShadowAnimation.finished_anim == true:
 			get_tree().change_scene("res://Menus/Title.tscn")
+	if PlayerStats.escaped == true:
+		$CenterContainer/HBoxContainer/VBoxContainer/CenterContainer/Escaped/TextureRect.set_texture(Escaped)
+	if PlayerStats.potato == true:
+		$CenterContainer/HBoxContainer/VBoxContainer/CenterContainer3/Potato/TextureButton.set_normal_texture(Potato)
+	if PlayerStats.missed == true:
+		$CenterContainer/HBoxContainer/VBoxContainer/CenterContainer2/You_missed/TextureRect.set_texture(Missed)
 
 func _ready():
 	ShadowAnimation.new_animation = "Opening"
@@ -19,4 +28,28 @@ func _on_Button_pressed():
 
 
 func _on_Escaped_mouse_entered():
-	pass # Replace with function body.
+	$CenterContainer/HBoxContainer/VBoxContainer/Label2.set_visible(true)
+
+
+func _on_Escaped_mouse_exited():
+	$CenterContainer/HBoxContainer/VBoxContainer/Label2.set_visible(false)
+
+
+func _on_Potato_mouse_entered():
+	$CenterContainer/HBoxContainer/VBoxContainer/Label3.set_visible(true)
+
+
+func _on_Potato_mouse_exited():
+	$CenterContainer/HBoxContainer/VBoxContainer/Label3.set_visible(false)
+
+
+func _on_You_missed_mouse_entered():
+	$CenterContainer/HBoxContainer/VBoxContainer/Label4.set_visible(true)
+
+
+func _on_You_missed_mouse_exited():
+	$CenterContainer/HBoxContainer/VBoxContainer/Label4.set_visible(false)
+
+
+func _on_TextureButton_pressed():
+	PlayerStats.missed = true
--- player/player_stats.gd
+++ player/player_stats.gd
@@ -7,12 +7,17 @@ var number_held = 0
 var can_pick = true
 var ui_block = false
 
+var escaped = false
+var potato = false
+var missed = false
+
 
 func change_level():
 	current_level += 1
 	if current_level <= level_count:
 		get_tree().change_scene("res://Levels/game_levels/Level_" +str(current_level)+".tscn")
 	else:
+		escaped = true
 		get_tree().change_scene("res://Menus/Title.tscn")
 
 func level_reset():
