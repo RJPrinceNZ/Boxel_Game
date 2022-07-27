ae4ac3528bc218794dc08b8d1721441de0da49c5
RJPrinceNZ <rjprincenz@gmail.com>
2022-07-27 12:23:56 +1200

    Level_13 Start

---
+++ levels/game_levels/Level_13.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _ready():
+	ShadowAnimation.new_animation = "Opening"
+	ShadowAnimation.change_animation()
+	PlayerStats.current_level = 13
+	PlayerStats.has_gun = true
+	GameMenu.active = true
+	#if not SoundPlayer.current_track == "Forward":
+		#SoundPlayer.change_music("Forward")
+	if PlayerStats.in_level == false:
+		PlayerStats.in_level = true
+		MusicPlayer.play_music()
+
+func _process(delta):
+	if PlayerStats.dark_background == true:
+		$Background.modulate = Color(0.4,0.4,0.4)
+		$SupportPland_decals.modulate = Color(0.4,0.4,0.4)
+	if PlayerStats.dark_background == false:
+		$Background.modulate = Color(1,1,1)
+		$SupportPland_decals.modulate = Color(1,1,1)
